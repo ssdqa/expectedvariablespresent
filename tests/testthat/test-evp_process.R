f73fa37d77da7fa27d02b5de858a1be862c7500c
wieandk <wieandk@chop.edu>
2024-10-31 11:07:30 -0400

    add tests w RSQLite

--- tests/testthat/test-evp_process.R
+++ tests/testthat/test-evp_process.R
@@ -42,25 +42,144 @@ test_that('only omop & pcornet are allowed inputs', {
 
 
 ## Generally checking that code runs
-test_that('testing omop version', {
+test_that('evp ss/ms exp nt -- omop', {
 
-  cht <- data.frame('person_id' = c(1000, 1001),
-                    'site' = c('a', 'b'),
-                    'start_date' = c(as.Date('2007-01-01'),as.Date('2008-01-01')),
-                    'end_date' = c(as.Date('2011-01-01'),as.Date('2009-01-01')))
+  rlang::is_installed("DBI")
+  rlang::is_installed("readr")
+  rlang::is_installed('RSQLite')
 
-  sess <- argos$new()
+  conn <- mk_testdb_omop()
 
-  set_argos_default(sess)
+  initialize_dq_session(session_name = 'evp_process_test',
+                        working_directory = getwd(),
+                        db_conn = conn,
+                        is_json = FALSE,
+                        file_subdirectory = 'testspecs',
+                        cdm_schema = NA)
 
-  config('results_name_tag', '')
+  cohort <- cdm_tbl('person') %>% distinct(person_id) %>%
+    mutate(start_date = as.Date(-5000),
+           end_date = as.Date(15000),
+           site = ifelse(person_id %in% c(1:6), 'synth1', 'synth2'))
 
-  expect_error(evp_process(cohort = cht,
-                          multi_or_single_site = 'single',
-                          anomaly_or_exploratory = 'exploratory',
-                          omop_or_pcornet = 'omop'))
+  evp_vars <- tidyr::tibble('variable' = 'hypertension',
+                            'domain_tbl' = 'condition_occurrence',
+                            'concept_field' = 'condition_concept_id',
+                            'date_field' = 'condition_start_date',
+                            'codeset_name' = 'dx_hypertension',
+                            'filter_logic' = NA)
+
+  expect_no_error(evp_process(cohort = cohort,
+                              evp_variable_file = evp_vars,
+                              multi_or_single_site = 'single',
+                              anomaly_or_exploratory = 'exploratory',
+                              omop_or_pcornet = 'omop'))
 })
 
+test_that('evp ss anom nt -- omop', {
+
+  rlang::is_installed("DBI")
+  rlang::is_installed("readr")
+  rlang::is_installed('RSQLite')
+
+  conn <- mk_testdb_omop()
+
+  initialize_dq_session(session_name = 'evp_process_test',
+                        working_directory = getwd(),
+                        db_conn = conn,
+                        is_json = FALSE,
+                        file_subdirectory = 'testspecs',
+                        cdm_schema = NA)
+
+  cohort <- cdm_tbl('person') %>% distinct(person_id) %>%
+    mutate(start_date = as.Date(-5000),
+           end_date = as.Date(15000),
+           site = ifelse(person_id %in% c(1:6), 'synth1', 'synth2'))
+
+  evp_vars <- tidyr::tibble('variable' = 'hypertension',
+                            'domain_tbl' = 'condition_occurrence',
+                            'concept_field' = 'condition_concept_id',
+                            'date_field' = 'condition_start_date',
+                            'codeset_name' = 'dx_hypertension',
+                            'filter_logic' = NA)
+
+  expect_no_error(evp_process(cohort = cohort,
+                              evp_variable_file = evp_vars,
+                              multi_or_single_site = 'single',
+                              anomaly_or_exploratory = 'anomaly',
+                              omop_or_pcornet = 'omop'))
+})
+
+test_that('evp ms anom nt -- omop', {
+
+  rlang::is_installed("DBI")
+  rlang::is_installed("readr")
+  rlang::is_installed('RSQLite')
+
+  conn <- mk_testdb_omop()
+
+  initialize_dq_session(session_name = 'evp_process_test',
+                        working_directory = getwd(),
+                        db_conn = conn,
+                        is_json = FALSE,
+                        file_subdirectory = 'testspecs',
+                        cdm_schema = NA)
+
+  cohort <- cdm_tbl('person') %>% distinct(person_id) %>%
+    mutate(start_date = as.Date(-5000),
+           end_date = as.Date(15000),
+           site = ifelse(person_id %in% c(1:6), 'synth1', 'synth2'))
+
+  evp_vars <- tidyr::tibble('variable' = 'hypertension',
+                            'domain_tbl' = 'condition_occurrence',
+                            'concept_field' = 'condition_concept_id',
+                            'date_field' = 'condition_start_date',
+                            'codeset_name' = 'dx_hypertension',
+                            'filter_logic' = NA)
+
+  expect_no_error(evp_process(cohort = cohort,
+                              evp_variable_file = evp_vars,
+                              multi_or_single_site = 'multi',
+                              anomaly_or_exploratory = 'anomaly',
+                              omop_or_pcornet = 'omop'))
+})
+
+test_that('evp ss/ms exp at -- omop', {
+
+  rlang::is_installed("DBI")
+  rlang::is_installed("readr")
+  rlang::is_installed('RSQLite')
+
+  conn <- mk_testdb_omop()
+
+  initialize_dq_session(session_name = 'evp_process_test',
+                        working_directory = getwd(),
+                        db_conn = conn,
+                        is_json = FALSE,
+                        file_subdirectory = 'testspecs',
+                        cdm_schema = NA)
+
+  cohort <- cdm_tbl('person') %>% distinct(person_id) %>%
+    mutate(start_date = as.Date(-5000),
+           end_date = as.Date(15000),
+           site = ifelse(person_id %in% c(1:6), 'synth1', 'synth2'))
+
+  evp_vars <- tidyr::tibble('variable' = 'hypertension',
+                            'domain_tbl' = 'condition_occurrence',
+                            'concept_field' = 'condition_concept_id',
+                            'date_field' = 'condition_start_date',
+                            'codeset_name' = 'dx_hypertension',
+                            'filter_logic' = NA)
+
+  expect_no_error(evp_process(cohort = cohort,
+                              evp_variable_file = evp_vars,
+                              multi_or_single_site = 'single',
+                              anomaly_or_exploratory = 'exploratory',
+                              time = TRUE,
+                              time_period = 'year',
+                              time_span = c('2000-01-01', '2005-01-01'),
+                              omop_or_pcornet = 'omop'))
+})
 
 test_that('testing pcornet version', {
 
